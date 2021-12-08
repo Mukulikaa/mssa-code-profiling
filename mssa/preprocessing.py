fee4d08280cf633c682c18e05bd2734a5df714a3
Mukulika <mukulikapahari@gmail.com>
2021-12-08 14:09:23 +0530

    feat: added moving_mean to preprocessing

--- mssa/preprocessing.py
+++ mssa/preprocessing.py
@@ -21,3 +21,36 @@ def unstack(data):
 
 def centre(data):
     return data - data.mean()
+
+# Step 4: Centered moving mean
+
+def moving_mean(a, nave, dim, opt=0):
+    """
+    Parameters
+    ----------
+    a : array_like
+        The object to be converted.
+    nave : int, required
+        Size of window.
+    dim : str, required
+        The dimension across which the moving mean has to be computed.
+    opt : [-1, 0]
+        -1 utilizes circular conditions.
+        0 (default) handles edge cases by replacing them with NaNs.
+
+    Notes
+    -----
+    For opt=-1, we backcast and forecast the timeseries to compute the moving
+    means for the edge cases.
+    """
+
+    if opt == -1:
+        n = nave // 2
+        if nave % 2 == 1:
+            temp = xr.concat((a[-n:], a, a[:n]), dim=dim)
+            return temp.rolling(dim={dim:nave}, center=True).mean().dropna(dim)
+        else:
+            temp = xr.concat((a[-n:], a, a[:(n - 1)]), dim=dim)
+            return temp.rolling(dim={dim:nave}, center=True).mean().dropna(dim)
+    elif opt == 0:
+        return a.rolling(time=nave, center=True).mean()
